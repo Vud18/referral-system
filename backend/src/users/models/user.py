8831549a9da5167a6bf349b57c422e021cc0287b
Vud <vud150294@gmail.com>
2024-10-23 23:59:08 +0500

    dev: Добавлена модель UserModel.

---
+++ backend/src/users/models/user.py
@@ -0,0 +1,23 @@
+import uuid
+
+from sqlalchemy import String
+from sqlalchemy import String as UUID
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from src.database import Base
+
+__all__ = ["UserModel"]
+
+
+class UserModel(Base):
+    """Основная модель пользователей.вфыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыыфыввввввввввввввввввввввввввввввввввввв"""
+
+    __tablename__ = "users"
+
+    id: Mapped[str] = mapped_column(
+        String, primary_key=True, index=True, default=str(uuid.uuid4())
+    )
+    username: Mapped[str] = mapped_column(unique=True, index=True)
+    email: Mapped[str] = mapped_column(unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column(nullable=True)
+    referrer_code: Mapped[str] = mapped_column(nullable=True)
+    referral_code = relationship("ReferralCode", back_populates="owner")
